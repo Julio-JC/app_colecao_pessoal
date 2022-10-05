7580d3ed711c921f7b4c58c34dd141ef0228d67f
Julio Cesar de Jesus <cesar.juliojc007@gmail.com>
2022-10-04 22:37:05 -0300

    Criado pasta e arquivo de projeto.

---
+++ app_colecao_pessoal/android/app/src/main/kotlin/com/example/app_colecao_pessoal/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_colecao_pessoal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app_colecao_pessoal/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'my_home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
